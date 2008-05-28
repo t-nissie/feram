e11c8b5eb33bc19b2cfbd55cb4e6eb7294f22278
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2008-05-28 08:51:08 +0000

    src/01example-BaTiO3-epit-hysteresis-loop/externalE.sh: Changed "        " (8 spaces) to a TAB for <<- in the shell script
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@245 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/01example-BaTiO3-epit-hysteresis-loop/externalE.sh
+++ src/01example-BaTiO3-epit-hysteresis-loop/externalE.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # externalE.sh
-# Time-stamp: <2008-02-06 14:27:18 takeshi>
+# Time-stamp: <2008-05-28 17:48:46 takeshi>
 # Author: Takeshi NISHIMATSU
 ##
 rm -f externalE.avg
@@ -30,51 +30,51 @@ while [ `perl -e "print $externalE <= $externalE_goal || 0"` = "1" ] ; do
     i=`expr $i + 1`
     filename=externalE"$temperature"K`printf '%.3d%+.5f' $i $externalE`
     cat > $filename <<-EOF
-        #--- Method, Temperature, and mass ---------------
-        method = 'md'
-        GPa = -5.0
-        kelvin = $temperature
-        mass_amu = 39.0
-        Q_Nose = 0.05
-        
-        #--- System geometry -----------------------------
-        bulk_or_film = 'epit'
-        L = 16 16 32
-        gap = 1
-        a0 =  3.94         latice constant a0 [Angstrom]
-        epi_strain = -0.01
-        #--- Time step -----------------------------------
-        dt = $dt [pico second]
-        n_thermalize = $n_thermalize
-        n_average    = $n_average
-        n_coord_freq = $n_coord_freq
-        
-        #--- External electric field ---------------------
-        external_E_field = 0.00 0.00 $externalE
-        
-        #--- On-site (Polynomial of order 4) -------------
-        P_kappa2 =    5.502  [eV/Angstrom^2] # P_4(u) = kappa2*u^2 + alpha*u^4
-        P_alpha  =  110.4    [eV/Angstrom^4] #  + gamma*(u_y*u_z+u_z*u_x+u_x*u_y),
-        P_gamma  = -163.1    [eV/Angstrom^4] # where u^2 = u_x^2 + u_y^2 + u_z^2
-        
-        #--- Inter-site ----------------------------------
-        j = -2.648 3.894 0.898 -0.789 0.562 0.358 0.179    j(i) [eV/Angstrom^2]
-        
-        #--- Elastic Constants ---------------------------
-        B11 = 126.
-        B12 =  44.9
-        B44 =  50.3  [eV]
-        
-        #--- Elastic Coupling ----------------------------
-        B1xx = -211.   [eV/Angstrom^2]
-        B1yy =  -19.3  [eV/Angstrom^2]
-        B4yz =   -7.75 [eV/Angstrom^2]
-        
-        #--- Dipole --------------------------------------
-        init_dipo_avg = 0.00  0.00 -0.10   [Angstrom]  # Average   of initial dipole displacements
-        init_dipo_dev = 0.02  0.02  0.02   [Angstrom]  # Deviation of initial dipole displacements
-        Z_star        = 9.956
-        epsilon_inf   = 5.24
+	#--- Method, Temperature, and mass ---------------
+	method = 'md'
+	GPa = -5.0
+	kelvin = $temperature
+	mass_amu = 39.0
+	Q_Nose = 0.05
+	
+	#--- System geometry -----------------------------
+	bulk_or_film = 'epit'
+	L = 16 16 32
+	gap = 1
+	a0 =  3.94         latice constant a0 [Angstrom]
+	epi_strain = -0.01
+	#--- Time step -----------------------------------
+	dt = $dt [pico second]
+	n_thermalize = $n_thermalize
+	n_average    = $n_average
+	n_coord_freq = $n_coord_freq
+	
+	#--- External electric field ---------------------
+	external_E_field = 0.00 0.00 $externalE
+	
+	#--- On-site (Polynomial of order 4) -------------
+	P_kappa2 =    5.502  [eV/Angstrom^2] # P_4(u) = kappa2*u^2 + alpha*u^4
+	P_alpha  =  110.4    [eV/Angstrom^4] #  + gamma*(u_y*u_z+u_z*u_x+u_x*u_y),
+	P_gamma  = -163.1    [eV/Angstrom^4] # where u^2 = u_x^2 + u_y^2 + u_z^2
+	
+	#--- Inter-site ----------------------------------
+	j = -2.648 3.894 0.898 -0.789 0.562 0.358 0.179    j(i) [eV/Angstrom^2]
+	
+	#--- Elastic Constants ---------------------------
+	B11 = 126.
+	B12 =  44.9
+	B44 =  50.3  [eV]
+	
+	#--- Elastic Coupling ----------------------------
+	B1xx = -211.   [eV/Angstrom^2]
+	B1yy =  -19.3  [eV/Angstrom^2]
+	B4yz =   -7.75 [eV/Angstrom^2]
+	
+	#--- Dipole --------------------------------------
+	init_dipo_avg = 0.00  0.00 -0.10   [Angstrom]  # Average   of initial dipole displacements
+	init_dipo_dev = 0.02  0.02  0.02   [Angstrom]  # Deviation of initial dipole displacements
+	Z_star        = 9.956
+	epsilon_inf   = 5.24
 EOF
     echo 1 > FILES
     echo $filename >> FILES
