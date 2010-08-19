005292d33bef7e6bbec92a90091782353349faa6
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2010-08-19 04:42:30 +0000

    utils/: deleted
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@583 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- utils/temperature.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-t=50
-while [ $t -le 170 ]; do
-    directory=`printf "%3.3i" $t`K
-    mkdir --verbose $directory 
-    sed -e "s/.*temperature \[K\]/$t.0/" temperature.in > $directory/$directory.in
-    ../feram < $directory/$directory.in > $directory/feram.out
-    t=`expr $t + 5`
-done
