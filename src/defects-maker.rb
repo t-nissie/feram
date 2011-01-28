7a642f7d7d7a65543fa48769faee7a543280bad5
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2011-01-28 07:57:39 +0000

    defects-maker.rb: random direction
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@688 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/defects-maker.rb
+++ src/defects-maker.rb
@@ -1,12 +1,24 @@
 #!/usr/bin/env ruby
 # defects-maker.rb
-# Time-stamp: <2011-01-07 18:14:58 takeshi>
+# Time-stamp: <2011-01-28 16:56:54 takeshi>
 # Author: Takeshi NISHIMATSU
 # Usage:   ./defects-maker.rb Lx Ly Lz fixed_dipole
 # Example: ./defects-maker.rb 16 16 81 '0.00 0.00 0.05'
+# Example: ./defects-maker.rb 16 16 81 'rand 0.05'
 ##
 (0...ARGV[0].to_i).each do |ix|
   (0...ARGV[1].to_i).each do |iy|
-    printf("%3d %3d %3d   %s\n", ix, iy, rand(ARGV[2].to_i/2), ARGV[3])
+    randp, absu = ARGV[3].strip.split
+    if randp=='rand' then
+      absu = absu.to_f
+      theta=Math::PI*rand()
+      phi=2*Math::PI*rand()
+      u=sprintf("%10.5f%10.5f%10.5f", absu*Math::sin(theta)*Math::cos(phi),
+                                      absu*Math::sin(theta)*Math::sin(phi),
+                                      absu*Math::cos(theta) )
+    else
+      u=ARGV[3]
+    end
+    printf("%3d %3d %3d   %s\n", ix, iy, rand(ARGV[2].to_i/2), u)
   end
 end
