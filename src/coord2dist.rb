145f9ebdf5ed7db2949766726eaae682c12dacde
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2012-03-21 03:18:05 +0000

    Now, coord2dist.rb can read data from many files. Use -r RANGE and -s STEP to specify range and step, respectively.
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@1132 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/coord2dist.rb
+++ src/coord2dist.rb
@@ -1,46 +1,48 @@
 #!/usr/bin/env ruby
 # coord2dist.rb
-# Time-stamp: <2012-03-17 15:42:01 takeshi>
+# Time-stamp: <2012-03-21 08:46:10 takeshi>
 # Author: Takeshi NISHIMATSU
-# Usage: ./coord2dist.rb coord-file [min] [max] [step]
+# Usage: ./coord2dist.rb --help
+#        ./coord2dist.rb -r 0.5 -s 0.1 coord_file1 coord_file2 coord_file3 ...
 ##
 
 # Default values
-min  = -0.4
-max  =  0.4
-step =  0.01
+range =  0.4
+step  =  0.01
 
-coord_file = ARGV[0]
-min  = ARGV[1].to_f if ARGV[1]
-max  = ARGV[2].to_f if ARGV[2]
-step = ARGV[3].to_f if ARGV[3]
+# Command arguments
+require "optparse"
+opts = OptionParser.new
+opts.on("-r RANGE","--range RANGE",
+        "Distribution will be counted from -RANGE to REANGE"){|v| range = v.to_f}
+opts.on("-s STEP","--step STEP",
+        "Distribution will be counted in STEP"){|v| step=v.to_f}
+opts.parse!(ARGV)
 
-n=((max-min)/step).floor+1
+# Prepare three arrays
+n=(2*range/step).floor+1
 ary_x = Array.new(n, 0)
 ary_y = Array.new(n, 0)
 ary_z = Array.new(n, 0)
 
-File.open(coord_file, "r"){|c|
-  c.each do |line|
-    x,y,z,q_x,q_y,q_z = line.strip.split
-    ix = ((q_x.to_f-min+step/2)/step).floor
-    iy = ((q_y.to_f-min+step/2)/step).floor
-    iz = ((q_z.to_f-min+step/2)/step).floor
-    ary_x[ix] += 1 if (0...n).include?(ix)
-    ary_y[iy] += 1 if (0...n).include?(iy)
-    ary_z[iz] += 1 if (0...n).include?(iz)
-  end
+# Read from files
+ARGF.each {|line|
+  x,y,z,q_x,q_y,q_z = line.strip.split
+  ix = ((q_x.to_f+range+step/2)/step).floor
+  iy = ((q_y.to_f+range+step/2)/step).floor
+  iz = ((q_z.to_f+range+step/2)/step).floor
+  ary_x[ix] += 1 if (0...n).include?(ix)
+  ary_y[iy] += 1 if (0...n).include?(iy)
+  ary_z[iz] += 1 if (0...n).include?(iz)
 }
 
-filename = File.basename(coord_file, ".coord")+".dist"
-File.open(filename, "w"){|c|
-  u = min
-  n.times{|i|
-    c.printf("%8.5f %10d %10d %10d\n", u,ary_x[i],ary_y[i],ary_z[i])
-    u += step
-  }
+# Write to STDOUT
+u = -range
+n.times{|i|
+  printf("%9.5f %10d %10d %10d\n", u,ary_x[i],ary_y[i],ary_z[i])
+  u += step
 }
 
 #Local variables:
-#  compile-command: "ruby coord2dist.rb coord2dist.coord -0.5 0.5 0.1 && cat coord2dist.dist"
+#  compile-command: "ruby coord2dist.rb -r 0.5 -s 0.1 coord2dist.coord > coord2dist.dist && cat coord2dist.dist"
 #End:
