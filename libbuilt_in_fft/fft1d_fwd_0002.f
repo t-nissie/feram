985110fd034e6ec4c093fb6dd75b12faabf5197e
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2012-05-31 02:10:21 +0000

    rename fft subroutines, delete libbuilt_in_fft
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@1289 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- libbuilt_in_fft/fft1d_fwd_0002.f
+++
@@ -1,9 +0,0 @@
-subroutine fft1d_fwd_0002(c,increment)
-  implicit none
-  complex*16, intent(inout) :: c(0:*)
-  integer,    intent(in)    :: increment
-  complex*16  tmp
-  tmp = (1.0d0,0.0d0) * c(1*increment)
-  c(1*increment) = c(0*increment) - tmp
-  c(0*increment) = c(0*increment) + tmp
-end subroutine fft1d_fwd_0002
