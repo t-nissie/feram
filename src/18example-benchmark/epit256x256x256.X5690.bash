c4b9257a0cf3487e200dda3dc3d64aad4a4a5306
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2012-08-09 18:21:16 +0000

    6 and 12 threads
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@1452 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

---
+++ src/18example-benchmark/epit256x256x256.X5690.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+##
+rm -f wisdom
+for i in 6 12; do
+    export OMP_NUM_THREADS=$i
+    taskset -c 0-`expr $i - 1` ../fft_check 100  256 256 256
+    mv wisdom_new wisdom
+    taskset -c 0-`expr $i - 1` ../feram epit256x256x256.feram 
+    timing_report=`grep TIMING_REPORT epit256x256x256.log`
+    printf '%2d %s\n' $s $i "$timing_report" >> epit256x256x256.X5690.dat
+    mv wisdom wisdom.256x256x256.threads$i
+done
+rm -f *.coord
