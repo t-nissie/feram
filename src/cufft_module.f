d162e97d84e067b6095b3994575ee69a35198ac4
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2013-12-21 05:53:50 +0000

    timing
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2019 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/cufft_module.f
+++ src/cufft_module.f
@@ -1,5 +1,5 @@
 ! cufft_module.f -*-f90-*-
-! Time-stamp: <2013-12-19 23:02:31 t-nissie>
+! Time-stamp: <2013-12-21 13:58:06 t-nissie>
 ! Author: Takeshi NISHIMATSU
 ! Reference: http://www.softek.co.jp/SPG/Pgi/TIPS/public/accel/cufft.html
 !!
@@ -78,6 +78,16 @@ module cufft_module
        integer(c_int), value :: plan, direction
        type(c_ptr),value     :: id, od
      end function cufftExecD2Z
+  end interface
+
+  interface cufftExecZ2D
+     function cufftExecZ2D(plan,id,od,direction) bind(C,name='cufftExecZ2D')
+       use, intrinsic :: iso_c_binding
+       implicit none
+       integer(c_int)        :: cufftExecZ2D
+       integer(c_int), value :: plan, direction
+       type(c_ptr),value     :: id, od
+     end function cufftExecZ2D
   end interface
 
   interface cublasDscal
