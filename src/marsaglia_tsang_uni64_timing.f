72813105b111d708159de6cca2adfdd1213ff93d
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-03-03 05:38:18 +0000

    ifort makes normal_dist() twice faster
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2075 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/marsaglia_tsang_uni64_timing.f
+++ src/marsaglia_tsang_uni64_timing.f
@@ -1,13 +1,15 @@
 ! marsaglia_tsang_uni64_timing.f -*-f90-*-
-! Time-stamp: <2014-03-03 09:28:38 takeshi>
+! Time-stamp: <2014-03-03 14:11:23 takeshi>
 ! Author: Takeshi NISHIMATSU
 ! Results:
-!  Machine                    RNG[M/s]  N()[M/s] date
-!  SR16000 (POWER7, 3.83GHz)  263.158    18.727  2014-03-03
-!  Xeon E5-2637 v2  3.50GHz    91.853    16.617  2014-03-03
-!  Xeon E5-2680     2.70GHz    84.239    14.651  2014-03-03
-!  Xeon X5650       2.67GHz    71.659    12.102  2014-03-03
-!  Celeron 1037U    1.80GHz    45.510     7.407  2014-03-03
+!  Machine                    RNG[M/s]  N()[M/s] date       compiler
+!  SR16000 (POWER7, 3.83GHz)  263.158    18.727  2014-03-03 IBM XL Fortran for AIX, V14.1
+!  Xeon E5-2637 v2  3.50GHz    88.169    37.164  2014-03-03 ifort 13.1.3
+!  Xeon E5-2637 v2  3.50GHz    91.853    16.617  2014-03-03 gfortran 4.4.7
+!  Xeon E5-2680     2.70GHz    84.239    14.651  2014-03-03 gfortran 4.4.7
+!  Xeon X5650       2.67GHz    64.470    25.254  2014-03-03 ifort 12.1.5
+!  Xeon X5650       2.67GHz    71.659    12.102  2014-03-03 gfortran 4.8.2
+!  Celeron 1037U    1.80GHz    45.510     7.407  2014-03-03 gfortran 4.8.1
 !!
 program marsaglia_tsang_uni64_timing
   use marsaglia_tsang_uni64_module
