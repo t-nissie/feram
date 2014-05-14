118fce51b21b526088d3aee53be1e543b09c16c6
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-05-14 16:47:36 +0000

    fix random placement on a sphere
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2149 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/defects-maker.rb
+++ src/defects-maker.rb
@@ -1,21 +1,26 @@
 #!/usr/bin/env ruby
 # defects-maker.rb
-# Time-stamp: <2011-02-09 19:16:33 takeshi>
+# Time-stamp: <2014-05-14 18:39:41 takeshi>
 # Author: Takeshi NISHIMATSU
-# Usage:   ./defects-maker.rb Lx Ly Lz fixed_dipole n_defects
+# Usage:   ./defects-maker.rb Lx Ly Lz fixed_dipole n_defects [seed]
 # Example: ./defects-maker.rb 16 16 81 '0.00 0.00 0.05' 100
-# Example: ./defects-maker.rb 16 16 81 'rand 0.05' 100
+# Example: ./defects-maker.rb 16 16 81 'rand 0.05'      100 2134567890
 ##
-(1..ARGV[4].to_i).each do |ix|
-  randp, absu = ARGV[3].strip.split
+randp, absu = ARGV[3].strip.split
 
+if ARGV[5] then
+  srand(ARGV[5].to_i)
+else
+  srand(1234567890)
+end
+
+(1..ARGV[4].to_i).each do |ix|
   if randp=='rand' then
-    absu = absu.to_f
-    theta=Math::PI*rand()
-    phi=2*Math::PI*rand()
-    u=sprintf("%10.5f%10.5f%10.5f", absu*Math::sin(theta)*Math::cos(phi),
-                                    absu*Math::sin(theta)*Math::sin(phi),
-                                    absu*Math::cos(theta) )
+    z   = rand(-1.0..1.0)
+    phi = rand(0...2*Math::PI)
+    u=sprintf("%10.5f%10.5f%10.5f", absu.to_f * Math::sqrt(1.0-z**2) * Math::cos(phi),
+                                    absu.to_f * Math::sqrt(1.0-z**2) * Math::sin(phi),
+                                    absu.to_f * z )
   else
     u=ARGV[3]
   end
