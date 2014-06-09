eb4b40c2ee676c6fb8f7d3fa27a86b3efb1fb79c
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-06-09 07:08:21 +0000

    filename was changed
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2177 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/defects_maker_check.sh
+++ src/defects_maker_check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# defect-maker-check.sh
+# defect_maker_check.sh
 ##
 $srcdir/defects_maker.rb 30 31 32 'rand 1.0' 20000 2134567890 > defects_maker_check.defects
 gnuplot -e 'basename="defects_maker_check"; binwidth=0.1' $srcdir/defects_maker_check.gp
