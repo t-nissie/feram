f235c835783e9ad3aa9db264e4b84c4878729634
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-06-20 03:26:18 +0000

    randomly placed points on a sphere
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2201 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/defects_maker_check.sh
+++ src/defects_maker_check.sh
@@ -3,5 +3,7 @@
 ##
 $srcdir/defects_maker.rb 30 31 32 'rand 1.0' 20000 2134567890 > defects_maker_check.defects
 gnuplot -e 'basename="defects_maker_check"; binwidth=0.1' $srcdir/defects_maker_check.gp
+gnuplot -e 'basename="defects_maker_check"; binwidth=1.8' $srcdir/defects_maker_check_sphere.gp
 epstopdf.pl defects_maker_check.eps
+epstopdf.pl defects_maker_check_sphere.eps
 test -z "`sort defects_maker_check.defects | colrm 15 | uniq -d`"
