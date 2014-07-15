5f298a6eb1fb5f6909fcffab6b022d635d2fe035
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-07-15 21:55:01 +0000

    and check it
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2241 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/date_and_time_module.f
+++ src/date_and_time_module.f
@@ -1,16 +1,21 @@
-! date_and_time2message.F -*-f90-*-
-! Time-stamp: <2013-10-30 19:10:35 takeshi>
+! date_and_time_module.f -*-f90-*-
+! Time-stamp: <2014-07-16 06:38:44 takeshi>
 ! Author: Takeshi NISHIMATSU
-! Purpose: Call internal date_and_time(), convert it into
-!          ISO 8601 format, then return the string.
 !!
-subroutine date_and_time2message(message)
+module date_and_time_module
   implicit none
-  character(len= 8) :: date
-  character(len=10) :: time
-  character(len= 5) :: zone
-  character(*), intent(out) :: message
-  call date_and_time(date, time, zone)
-  write(message,'(a,"-",a,"-",a,"T",a,":",a,":",a,a)') &
-       & date(1:4), date(5:6), date(7:8), time(1:2), time(3:4), time(5:10), zone
-end subroutine date_and_time2message
+  private
+  public :: date_and_time2message
+contains
+  subroutine date_and_time2message(message)
+    ! Call internal date_and_time(), convert it into ISO 8601 format, then return the string.
+    implicit none
+    character(len= 8) :: date
+    character(len=10) :: time
+    character(len= 5) :: zone
+    character(*), intent(out) :: message
+    call date_and_time(date, time, zone)
+    write(message,'(a,"-",a,"-",a,"T",a,":",a,":",a,a)') &
+         & date(1:4), date(5:6), date(7:8), time(1:2), time(3:4), time(5:10), zone
+  end subroutine date_and_time2message
+end module date_and_time_module
