c3c5a70e77cba8ccceeb64ae47ce6e995e721686
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-08-30 07:22:03 +0000

    feram will be ../
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2282 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/26example-BaTiO3-acoustic-MD/acoustic-MD.6threads.csh
+++ src/26example-BaTiO3-acoustic-MD/acoustic-MD.6threads.csh
@@ -4,8 +4,8 @@
 # Usage:   submit AEB general -exec acoustic-MD.csh -J acoustic-MD
 ##
 setenv OMP_NUM_THREADS 6
-taskset -c 6-11 ./feram acoustic-MD.vs.feram
+taskset -c 6-11 ../feram acoustic-MD.vs.feram
 ln -sf acoustic-MD.vs.0000050000.coord acoustic-MD.lf.restart
-taskset -c 6-11 ./feram acoustic-MD.lf.feram
+taskset -c 6-11 ../feram acoustic-MD.lf.feram
 grep TIMING_REPORT *.log > acoustic-MD.dat
 rm -f  acoustic-MD.vs.0000050000.coord acoustic-MD.lf.restart
--- src/26example-BaTiO3-acoustic-MD/scorep_08threads.sh
+++ src/26example-BaTiO3-acoustic-MD/scorep_08threads.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Usage: ./scorep_08threads.sh > scorep_08threads.out 2>&1 &
 ##
-OMP_NUM_THREADS=8 scan -e scorep_acoustic-MD.vs_08threads ./feram acoustic-MD.vs.feram
+OMP_NUM_THREADS=8 scan -e scorep_acoustic-MD.vs_08threads ../feram acoustic-MD.vs.feram
 mv acoustic-MD.vs.log     scorep_acoustic-MD.vs_08threads
 ln -sf acoustic-MD.vs.0000050000.coord acoustic-MD.lf.restart
-OMP_NUM_THREADS=8 scan -e scorep_acoustic-MD.lf_08threads ./feram acoustic-MD.lf.feram
+OMP_NUM_THREADS=8 scan -e scorep_acoustic-MD.lf_08threads ../feram acoustic-MD.lf.feram
 mv acoustic-MD.lf.log     scorep_acoustic-MD.lf_08threads
 rm -rf acoustic-MD.vs.0000050000.coord acoustic-MD.lf.restart
