c31185d3aa55e6fe0d5af53294e40597b4baf5f2
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2014-12-29 11:34:03 +0000

    for Windows with MinGW
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@2339 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/defects_maker_check.sh
+++ src/defects_maker_check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # defect_maker_check.sh
 ##
-$srcdir/defects_maker.rb 30 31 32 'rand 1.0' 20000 2134567890 > defects_maker_check.defects
+ruby $srcdir/defects_maker.rb 30 31 32 'rand 1.0' 20000 2134567890 > defects_maker_check.defects
 gnuplot -e 'basename="defects_maker_check"; binwidth=0.1' $srcdir/defects_maker_check.gp
 gnuplot -e 'basename="defects_maker_check"; binwidth=1.8' $srcdir/defects_maker_check_sphere.gp
 epstopdf.pl defects_maker_check.eps
