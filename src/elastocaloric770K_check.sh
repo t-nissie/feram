dfd8fa94f357fdaf9e8dde94531405b577910183
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2015-03-30 21:42:01 +0000

    avoid an error in `make distcheck`
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/branches/newplan@2407 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/elastocaloric770K_check.sh
+++ src/elastocaloric770K_check.sh
@@ -7,7 +7,7 @@ if [ -z "$XLSMPOPTS" ]; then
     export XLSMPOPTS="spins=0:yields=0"
 fi
 
-if [ ! -f elastocaloric770K_check.vs.feram ]; then
+if [ ! $srcdir = . ]; then
     cp $srcdir/elastocaloric770K_check.vs.feram .
     cp $srcdir/elastocaloric770K_check.lf.feram .
 fi
@@ -24,4 +24,9 @@ if [ `echo "691.00 < $t_final" | bc` = 1  -a `echo "$t_final < 695.00" | bc` = 1
 else
     echo $0 : T_final = $t_final seems NOT good
     exit 1
+fi
+
+if [ ! $srcdir = . ]; then
+    rm elastocaloric770K_check.vs.feram \
+       elastocaloric770K_check.lf.feram
 fi
