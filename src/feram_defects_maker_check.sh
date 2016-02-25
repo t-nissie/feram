0cfb4739f328acce21ae5ec522d97d5cb5efa382
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2016-02-25 11:37:14 +0000

    epstopdf.pl -> epstopdf
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/branches/z_star_r@2687 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/feram_defects_maker_check.sh
+++ src/feram_defects_maker_check.sh
@@ -4,6 +4,6 @@
 ruby $srcdir/feram_defects_maker.rb 30 31 32 'rand 1.0' 20000 2134567890 > feram_defects_maker_check.defects
 gnuplot -e 'basename="feram_defects_maker_check"; binwidth=0.1' $srcdir/feram_defects_maker_check.gp
 gnuplot -e 'basename="feram_defects_maker_check"; binwidth=1.8' $srcdir/feram_defects_maker_check_sphere.gp
-epstopdf.pl feram_defects_maker_check.eps
-epstopdf.pl feram_defects_maker_check_sphere.eps
+epstopdf feram_defects_maker_check.eps
+epstopdf feram_defects_maker_check_sphere.eps
 test -z "`sort feram_defects_maker_check.defects | cut -b -15 | uniq -d`"
