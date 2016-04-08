199e9e114f4bbc5090759c4ca74fa8a858d42fbe
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2016-04-08 08:54:37 +0000

    add "use decay_function_module"
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/branches/z_star_r@2752 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/decay_function_check.f
+++ src/decay_function_check.f
@@ -1,28 +1,31 @@
-! decay-test.f   -*-f90-*-
-! Time-stamp: <2005-08-29 17:06:38 t-nissie>
+! decay_fuction_check.f   -*-f90-*-
+! Time-stamp: <2016-04-08 17:52:51 takeshi>
 ! Author: Takeshi NISHIMATSU
-!
-! Example$ echo 4.0 16 0.22 | ./decay-test
-!
+! Usage:  ./decay_function_check 4.0 16 0.22
+! $Date: 2012-07-06 21:27:09 +0900 (金, 06  7 2012) $
+! $Rev: 1340 $
+! $Author: t-nissie $
 !!
-program decay_test
+program decay_function_check
+  use decay_function_module
   implicit none
   real*8   a0
   integer  L
   real*8   kappa
   integer,parameter :: max_real = 3
-  !--- etc -------------------------------------------
   integer     :: i
   real*8      :: r,rr
-  !--- decay functions -------------------------------
-  real*8   B_decay,Crr
-  external B_decay,Crr
-
-  read(5,*) a0, L, kappa
-
+  character(len=30) :: str
+  if (command_argument_count().ne.3) then
+     write(0,'(a,a,i2,a)') __FILE__, ':', __LINE__, ': ERROR: Three command-line arguments are required.'
+     stop 1
+  end if
+  call get_command_argument(1,str); read(str,*) a0
+  call get_command_argument(2,str); read(str,*) L
+  call get_command_argument(3,str); read(str,*) kappa
   do i=1,L*max_real
      r=i*a0
      rr=r**2
      write(6,'(i3,f6.1,2e16.7E3)') i, r, B_decay(r,rr,kappa), Crr(r,rr,kappa)
   end do
-end program decay_test
+end program decay_function_check
