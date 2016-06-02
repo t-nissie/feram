1637cbd038ee5470d7829b2f4190c02f97d71615
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2016-06-02 02:43:49 +0000

    10^8 normally distributed random numbers will be generated
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/branches/z_star_r@2826 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/marsaglia_tsang_uni64_timing.f
+++ src/marsaglia_tsang_uni64_timing.f
@@ -1,5 +1,5 @@
 ! marsaglia_tsang_uni64_timing.f -*-f90-*-
-! Time-stamp: <2014-03-08 21:14:01 takeshi>
+! Time-stamp: <2016-06-02 11:42:04 takeshi>
 ! Author: Takeshi NISHIMATSU
 ! Results:
 !  Machine                    RNG[M/s]  N()[M/s] date       compiler
@@ -11,21 +11,23 @@
 !  Xeon X5650       2.67GHz    71.659    12.102  2014-03-03 gfortran 4.8.2
 !  Celeron 1037U    1.80GHz    45.510     7.407  2014-03-03 gfortran 4.8.1
 !  Celeron ULV743   1.30GHz    32.977     4.751  2014-03-08 gfortran 4.7.2
+!  Core i5 3210M    2.50GHz    71.311    25.517  2016-06-02 gfortran 6.1.0
 !!
 program marsaglia_tsang_uni64_timing
   use marsaglia_tsang_uni64_module
   implicit none
-  integer, parameter :: N=1000*1000*1000
+  integer, parameter :: N9=1000*1000*1000
+  integer, parameter :: N8= 100*1000*1000
   integer :: i
   real*8  :: x
   integer :: count_start, count_goal, count_rate, count_max
   real*8  :: t
   if (uni64(123456789,987654321) .ne. 1.0d0) stop 1
 
-  write(6,'(a)') 'marsaglia_tsang_uni64_timing: Initialization has been done. Timing starts.'
+  write(6,'(a)') 'marsaglia_tsang_uni64_timing.f:27: Initialization has been done. Timing starts.'
 
   call system_clock(count_start)
-  do i=1,N
+  do i=1,N9
      x=uni64()
   end do
   call system_clock(count_goal, count_rate, count_max)
@@ -35,23 +37,23 @@ program marsaglia_tsang_uni64_timing
   t = dble(count_goal-count_start)/count_rate
   if (t<0.0d0) t=t+dble(count_max)/count_rate
   write(6,'(a,f10.3,a)') '10^9 double-precision random numbers were generated in    ', t, ' [s].'
-  write(6,'(a,f10.3,a)') 'Therefore, speed of this marsaglia_tsang_uni64 RNG is     ', dble(N)*1.0d-6/t, ' [M/s].'
+  write(6,'(a,f10.3,a)') 'Therefore, speed of this marsaglia_tsang_uni64 RNG is     ', dble(N9)*1.0d-6/t, ' [M/s].'
 
   if (normal_dist(50.0d0,10.0d0,123456798,987654312) .ne. 2.0d0) stop 3
 
-  write(6,'(a)') 'marsaglia_tsang_uni64_timing: Second initialization has been done. Timing starts.'
+  write(6,'(a)') 'marsaglia_tsang_uni64_timing.f:44: Second initialization has been done. Timing starts.'
 
   call system_clock(count_start)
-  do i=1,N
+  do i=1,N8
      x=normal_dist(50.0d0,10.0d0)
   end do
   call system_clock(count_goal)
 
   t = dble(count_goal-count_start)/count_rate
   if (t<0.0d0) t=t+dble(count_max)/count_rate
-  write(6,'(a,f10.3,a)') '10^9 normally distributed random numbers were generated in', t, ' [s].'
-  write(6,'(a,f10.3,a)') 'Therefore, speed of this normal_dist RNG is               ', dble(N)*1.0d-6/t, ' [M/s].'
+  write(6,'(a,f10.3,a)') '10^8 normally distributed random numbers were generated in', t, ' [s].'
+  write(6,'(a,f10.3,a)') 'Therefore, speed of this normal_dist RNG is               ', dble(N8)*1.0d-6/t, ' [M/s].'
 end program marsaglia_tsang_uni64_timing
 !local variables:
-!  compile-command: "make marsaglia_tsang_uni64_timing; ./marsaglia_tsang_uni64_timing"
+!  compile-command: "make check TESTS='marsaglia_tsang_uni64_timing' && cat marsaglia_tsang_uni64_timing.log"
 !End:
