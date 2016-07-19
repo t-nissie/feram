47633f0c7e8e77275ff7f6f2a41287fd820b0dc4
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2016-07-19 23:04:39 +0000

    for changed feram_transition_sorter.rb
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/branches/z_star_r@2874 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/feram_transition_check.sh
+++ src/feram_transition_check.sh
@@ -3,7 +3,7 @@
 ##
 result=`ruby $srcdir/feram_transition_detector.rb 0.05 $srcdir/21example-KNbO3/*ing.avg |\
         grep Tc | ruby $srcdir/feram_transition_sorter.rb`
-if [ "$result" != "136.00 216.00  278.00 370.00  694.00 714.00" ]; then
-    echo $0:$LINENO: Could not detect Tc.
+if [ "$result" != "136.00 216.00   278.00 370.00   694.00 714.00" ]; then
+    echo $0:$LINENO: Could not detect Tc correctly. The result was "$result".
     exit 1
 fi
