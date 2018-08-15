a46cb1288dfa63a87876619591436576e6ba7192
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2018-08-15 11:25:00 +0000

    add forgotten colon in the zone of ISO 8601 extended format
    git-svn-id: https://svn.code.sf.net/p/loto/code/feram/trunk@3023 66b5f4e2-d06a-45f9-b3fd-3971e49ba91e

--- src/date_and_time_module.f
+++ src/date_and_time_module.f
@@ -1,5 +1,5 @@
 ! date_and_time_module.f -*-f90-*-
-! Time-stamp: <2014-07-16 06:38:44 takeshi>
+! Time-stamp: <2018-08-15 20:15:20 takeshi>
 ! Author: Takeshi NISHIMATSU
 !!
 module date_and_time_module
@@ -8,14 +8,14 @@ module date_and_time_module
   public :: date_and_time2message
 contains
   subroutine date_and_time2message(message)
-    ! Call internal date_and_time(), convert it into ISO 8601 format, then return the string.
+    ! Call internal date_and_time(), convert it into ISO 8601 extended format, then return the string.
     implicit none
     character(len= 8) :: date
     character(len=10) :: time
     character(len= 5) :: zone
     character(*), intent(out) :: message
     call date_and_time(date, time, zone)
-    write(message,'(a,"-",a,"-",a,"T",a,":",a,":",a,a)') &
-         & date(1:4), date(5:6), date(7:8), time(1:2), time(3:4), time(5:10), zone
+    write(message,'(a,"-",a,"-",a,"T",a,":",a,":",a,a,":",a)') &
+         & date(1:4), date(5:6), date(7:8), time(1:2), time(3:4), time(5:10), zone(1:3), zone(4:5)
   end subroutine date_and_time2message
 end module date_and_time_module
